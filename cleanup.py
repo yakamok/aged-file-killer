35ebabc99654f0af8ab85bcdfa104c87bba6d264
yakamok <38737288+yakamok@users.noreply.github.com>
2022-02-07 19:45:50 +0000

    Update cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -15,12 +15,10 @@ WatchedLocation = "/path/dir/files*"
 if days.isnumeric():
     print("true")
 else:
-    print("shite")
     exit(0)
 
 ToLive = int(days) * int(86400)
 
-print(ToLive)
 def checkAgeOfFile(filename):
     fname = pathlib.Path(filename)
     
@@ -32,15 +30,11 @@ def checkAgeOfFile(filename):
 
 def deleteFilesNFolders(location):
     if os.path.isdir(location):
-        print("This is a dir: " + location)
         shutil.rmtree(location)
     if os.path.isfile(location):
-        print("This is a file: " + location)
         os.remove(location)
 
 
 for x in glob.glob(WatchedLocation):
-    print(x)
     if checkAgeOfFile(x):
-        print(x)
         deleteFilesNFolders(x)
